4a4ee9dbf2cc8ee4c33f139663748d0ee65e2f2e
ali <moh.ali.lakkis@gmail.com>
2021-07-18 23:34:27 +0300

    remove main

--- src/sample/Encryptor.java
+++ src/sample/Encryptor.java
@@ -3,7 +3,7 @@ package sample;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Scanner;
+
 
 public class Encryptor {
 
@@ -18,25 +18,5 @@ public class Encryptor {
         BigInteger bigInt = new BigInteger(1,messageDigest);
 
         return bigInt.toString(16);
-    }
-
-    public static void main(String[] args) throws NoSuchAlgorithmException {
-        Encryptor encryptor = new Encryptor();
-
-        String password = "monkey123";
-        String hashedPas = "cc25c0f861a83f5efadc6e1ba9d1269e";
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Hey! Plz input your Password: \n");
-
-        String userInput = scanner.nextLine();
-
-        if(encryptor.encryptString(userInput).equals(hashedPas)){
-            System.out.println("Correct! You are in!");
-        } else{
-            System.out.println("Wrong!!");
-        }
-
     }
 }
